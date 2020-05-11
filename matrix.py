afaa48e88936028d9726e7e038b0768252ff5d5a
G Naveen <naveengondu18@gmail.com>
2020-05-12 00:05:37 +0530

    mmatrix calculations

--- matrix.py
+++ matrix.py
@@ -9,7 +9,21 @@ class Matrix():
 			for j in range(self.cols):
 				self.matrix[i].append(0)
 
-	def Multiply(self, n):
+	# Here m1 and m2 are the matrix objects 
+	@staticmethod
+	def multiply(m1, m2):
+		# size of resulting array is m1.rows x m2.cols
+		result = list() 
+		if m1.cols == m2.rows:
+			for i in range(m1.rows):
+				for j in range(m2.cols):
+					for k in range(m1.rows):
+						d = m1.matrix[i][j+k] + m2.matrix[i+k][j]
+						print(d)
+		else:
+			print("Columns of m1 must be equal to the rows of m2.")
+	@classmethod
+	def multiply(self, n):
 		if isinstance(n, Matrix):
 			for i in range(self.rows):
 				for j in range(self.cols):
@@ -19,7 +33,7 @@ class Matrix():
 				for j in range(self.cols):
 					self.matrix[i][j] = self.matrix[i][j]*n
 
-	def Add(self, n):
+	def add(self, n):
 		if isinstance(n, Matrix):
 			for i in range(self.rows):
 				for j in range(self.cols):
