6da41f8830626af1875eb464296e078ef7837726
Nav3011 <naveengondu18@gmail.com>
2020-05-30 00:04:19 +0530

    added matrix operations

--- matrix.py
+++ matrix.py
@@ -1,21 +1,28 @@
-from random import randint
+from random import random
 # from random import seed
 # seed(1)
 class Matrix():
 	def __init__(self, rows, cols):
 		self.rows = rows
 		self.cols = cols
-		self.matrix = list()
+		self.data = list()
 
 		for i in range(self.rows):
-			self.matrix.append(list())
+			self.data.append(list())
 			for j in range(self.cols):
-				self.matrix[i].append(0)
+				self.data[i].append(0)
 
+	@staticmethod
+	def fromArray(arr):
+		res = Matrix(len(arr),1)
+		for i in range(len(arr)):
+			res.data[i][0] = arr[i]
+		return res
 	# Here m1 and m2 are the matrix objects 
 	@staticmethod
 	def dot(m1, m2):
 		# size of resulting array is m1.rows x m2.cols
+		res = Matrix(m1.rows, m2.cols)
 		result = list() 
 		if m1.cols == m2.rows:
 			for i in range(m1.rows):
@@ -23,40 +30,57 @@ class Matrix():
 				for j in range(m2.cols):
 					sum_ = 0
 					for k in range(m1.cols):
-						sum_ = sum_ + m1.matrix[i][k] * m2.matrix[k][j]
+						sum_ = sum_ + m1.data[i][k] * m2.data[k][j]
 					dt.append(sum_)
 				result.append(dt)
-			print(result)
+			for i in range(m1.rows):
+				for j in range(m2.cols):
+					res.data[i][j] = result[i][j]
+			return res
 		else:
 			print("Columns of m1 must be equal to the rows of m2.")
+
 	@classmethod
 	def multiply(self, n):
 		if isinstance(n, Matrix):
 			for i in range(self.rows):
 				for j in range(self.cols):
-					self.matrix[i][j] = self.matrix[i][j]*n.matrix[i][j]
+					self.data[i][j] = self.data[i][j]*n.data[i][j]
 		else:
 			for i in range(self.rows):
 				for j in range(self.cols):
-					self.matrix[i][j] = self.matrix[i][j]*n
+					self.data[i][j] = self.data[i][j]*n
 
 	def add(self, n):
 		if isinstance(n, Matrix):
 			for i in range(self.rows):
 				for j in range(self.cols):
-					self.matrix[i][j] = self.matrix[i][j]+n.matrix[i][j]
+					self.data[i][j] = self.data[i][j]+n.data[i][j]
 		else:
 			for i in range(self.rows):
 				for j in range(self.cols):
-					self.matrix[i][j] = self.matrix[i][j]+n
+					self.data[i][j] = self.data[i][j]+n
 
 
 	def randomize(self):
 		for i in range(self.rows):
 			for j in range(self.cols):
-				self.matrix[i][j] = randint(1,7)
+				self.data[i][j] = random()*2 - 1
 
 	def show(self):
 		for i in range(self.rows):
 			for j in range(self.cols):
-				print(self.matrix[i][j])
+				print(self.data[i][j])
+
+	def transpose(self):
+		result = Matrix(self.cols, self.rows)
+		for i in range(self.rows):
+			for j in range(self.cols):
+				result.data[j][i] = self.data[i][j]
+		return result.data
+
+	def map(self,func):
+		for i in range(self.rows):
+			for j in range(self.cols):
+				val = self.data[i][j]
+				self.data[i][j] = func(val)
