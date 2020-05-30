e58ee75cbd22f035a013d655060c5dd32a30a98a
Nav3011 <naveengondu18@gmail.com>
2020-05-31 01:09:23 +0530

    added subtract and map static function

--- matrix.py
+++ matrix.py
@@ -18,10 +18,9 @@ class Matrix():
 		for i in range(len(arr)):
 			res.data[i][0] = arr[i]
 		return res
-	# Here m1 and m2 are the matrix objects 
+
 	@staticmethod
 	def dot(m1, m2):
-		# size of resulting array is m1.rows x m2.cols
 		res = Matrix(m1.rows, m2.cols)
 		result = list() 
 		if m1.cols == m2.rows:
@@ -40,11 +39,11 @@ class Matrix():
 		else:
 			print("Columns of m1 must be equal to the rows of m2.")
 
-	@classmethod
+	# @classmethod
 	def multiply(self, n):
 		if isinstance(n, Matrix):
-			for i in range(self.rows):
-				for j in range(self.cols):
+			for i in range(n.rows):
+				for j in range(n.cols):
 					self.data[i][j] = self.data[i][j]*n.data[i][j]
 		else:
 			for i in range(self.rows):
@@ -61,6 +60,13 @@ class Matrix():
 				for j in range(self.cols):
 					self.data[i][j] = self.data[i][j]+n
 
+	@staticmethod
+	def subtract(m1, m2):
+		result = Matrix(m1.rows, m1.cols)
+		for i in range(m1.rows):
+			for j in range(m1.cols):
+				result.data[i][j] = m1.data[i][j] - m2.data[i][j]
+		return result
 
 	def randomize(self):
 		for i in range(self.rows):
@@ -72,15 +78,19 @@ class Matrix():
 			for j in range(self.cols):
 				print(self.data[i][j])
 
-	def transpose(self):
-		result = Matrix(self.cols, self.rows)
-		for i in range(self.rows):
-			for j in range(self.cols):
-				result.data[j][i] = self.data[i][j]
-		return result.data
+	@staticmethod
+	def transpose(m):
+		result = Matrix(m.cols, m.rows)
+		for i in range(m.rows):
+			for j in range(m.cols):
+				result.data[j][i] = m.data[i][j]
+		return result
 
-	def map(self,func):
-		for i in range(self.rows):
-			for j in range(self.cols):
-				val = self.data[i][j]
-				self.data[i][j] = func(val)
+	@staticmethod
+	def map(m, func):
+		result = Matrix(m.rows, m.cols)
+		for i in range(m.rows):
+			for j in range(m.cols):
+				val = m.data[i][j]
+				result.data[i][j] = func(val)
+		return result
