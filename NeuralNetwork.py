3bf158c42098b5399f29985f2471271c2c7da159
Nav3011 <naveengondu18@gmail.com>
2020-06-09 16:57:05 +0530

    Added backpropagation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,6 +3,10 @@ import math
 
 def sigmoid(x):
 	return 1 / (1 + math.exp(-x))
+
+def sigmoidD(x):
+	return sigmoid(x) * (1-sigmoid(x))
+
 class NeuralNetwork:
 
 	def __init__(self, inputLayer, hiddenLayer1, hiddenLayer2, outputLayer):
@@ -36,6 +40,7 @@ class NeuralNetwork:
 
 	def train(self, inputs, outputs):
 		#inputs and outputs are list. Convert to Matrix object
+		#Feed forward
 		self.Input = Matrix.fromArray(inputs)
 		self.Output = Matrix.fromArray(outputs)
 
@@ -50,3 +55,38 @@ class NeuralNetwork:
 		self.z3 = Matrix.dot(self.weights_3, self.a2)
 		self.z3.add(self.biases_3)
 		self.a3 = Matrix.map(self.z3, sigmoid)
+
+		self.loss = Matrix.subtract(self.a3, self.Output)
+		self.loss = Matrix.multiply(self.loss, self.loss)
+		self.loss = Matrix.multiply(self.loss, 0.5)
+
+		#Back propagation
+		
+		#W3 and bias3 gradient calculation 
+		self.sigmoidD3 = Matrix.map(self.z3, sigmoidD)
+		self.delta3 = Matrix.subtract(self.a3, self.Output)
+		self.delta3 = Matrix.multiply(self.delta3, self.sigmoidD3)
+		self.a2_transpose = Matrix.transpose(self.a2)
+		self.weight3_gradient = Matrix.dot(self.delta3, self.a2_transpose)
+		
+		#W2 and bias gradient calculation
+		self.sigmoidD2 = Matrix.map(self.z2, sigmoidD)
+		self.weight3_transpose = Matrix.transpose(self.weights_3)
+		self.delta2 = Matrix.dot(self.weight3_transpose, self.delta3)
+		self.delta2 = Matrix.multiply(self.delta2, self.sigmoidD2)
+		self.a1_transpose = Matrix.transpose(self.a1)
+		self.weight2_gradient = Matrix.dot(self.delta2, self.a1_transpose)
+		# print(self.weight2_gradient.data)
+
+		self.sigmoidD1 = Matrix.map(self.z1, sigmoidD)
+		self.weight2_transpose = Matrix.transpose(self.weights_2)
+		self.delta1 = Matrix.dot(self.weight2_transpose, self.delta2)
+		self.delta1 = Matrix.multiply(self.delta1, self.sigmoidD1)
+		self.input_transpose = Matrix.transpose(self.Input)
+		self.weight1_gradient = Matrix.dot(self.delta1, self.input_transpose)
+		# print(self.weight1_gradient.data)		
+
+
+
+
+
