9cbc929eec6dbd2bfe831820f877e7348951ad0c
Nav3011 <naveengondu18@gmail.com>
2020-06-09 16:55:21 +0530

    added output

--- driver.py
+++ driver.py
@@ -3,8 +3,7 @@ import random
 def main():
 	NN = NeuralNetwork(4,5,3,2)
 	inputs = [1,1,1,1]
-	outputs= [1]
-	result = NN.train(inputs, outputs)
-	print(result.data)
+	outputs= [1,1]
+	NN.train(inputs, outputs)
 if __name__ == '__main__':
 	main()
